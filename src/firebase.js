a18409163b56d84843ffaf5550993116f8f34002
Satej Rashinkar <satej.rashinkar@gmail.com>
2021-06-20 20:24:44 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlqyi7GCO9jc6TQuDYepz4pP-Ev5I38GM",
+    authDomain: "whatsapp-mern-28ffc.firebaseapp.com",
+    projectId: "whatsapp-mern-28ffc",
+    storageBucket: "whatsapp-mern-28ffc.appspot.com",
+    messagingSenderId: "212100501997",
+    appId: "1:212100501997:web:1ed927e973b6d3c47b17e4"
+  };
